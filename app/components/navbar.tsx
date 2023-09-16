4b34967b1fc174190fd2d76d4742272867024284
Wilson Guilherme Spindler <wilsongspindler@gmail.com>
2023-09-16 15:38:01 -0300

    creatred a navbar and store shoppingbag

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,9 +2,14 @@ import Link from "next/link";
 
 import Container from "./ui/container"
 import MainNav from "./main-nav";
+import getCategories from "@/actions/get-categories";
+import NavbarActions from "./navbar-actions";
 
+export const revalidate = 0;
 
-const Navbar = () => {
+const Navbar = async () => {
+    const categories = await getCategories();
+
     return ( 
         <div className="border-b">
             <Container>
@@ -12,7 +17,8 @@ const Navbar = () => {
                     <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
                         <p className="font-bold text-xl">STORE</p>
                     </Link>
-                    <MainNav/>
+                    <MainNav data={categories}/>
+                    <NavbarActions/>
                 </div>
             </Container>
         </div>
