eedf16e8bdc39d5fc93a063ab541058d1a8ef82e
Wilson Guilherme Spindler <wilsongspindler@gmail.com>
2023-09-16 11:02:03 -0300

    initial commit

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/guilherme/ecommerce-store/app/layout\":[\"static/media/01af0fc7b4278e65-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
