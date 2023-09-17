91c3dbe7c3f7e6e99935672b87015e2558d232f8
Wilson Guilherme Spindler <wilsongspindler@gmail.com>
2023-09-17 19:53:45 -0300

    adjusting billboard

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,31 +1,36 @@
-"use client"
+"use client";
 
 import { ShoppingBag } from "lucide-react";
-import Button from "./ui/button";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
+import Button from "@/components/ui/button";
+
 const NavbarActions = () => {
-    const [isMounted, setIsMounted] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
 
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-    if (!isMounted) return null;
+  const router = useRouter();
 
-    return ( 
-        <div className="ml-auto items-center gap-x-4">
-            <Button className="flex items-center rounded-full bg-black px-4 py-2">
-                <ShoppingBag
-                    size={20}
-                    color="white"
-                />
-                <span className="ml-2 text-sm font-medium text-white">
-                    0
-                </span>
-            </Button>
-        </div>
-     );
+  if (!isMounted) {
+    return null;
+  }
+
+  return ( 
+    <div className="ml-auto flex items-center gap-x-4">
+      <Button onClick={() => router.push('/cart')} className="flex items-center rounded-full bg-black px-4 py-2">
+        <ShoppingBag
+          size={20}
+          color="white"
+        />
+        <span className="ml-2 text-sm font-medium text-white">
+        </span>
+      </Button>
+    </div>
+  );
 }
  
 export default NavbarActions;
