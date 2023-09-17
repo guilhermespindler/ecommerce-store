8830539b6729ee5e760d6a7fc6127d80d0d84daa
Wilson Guilherme Spindler <wilsongspindler@gmail.com>
2023-09-17 19:49:50 -0300

    updating billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,16 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+import Container from "../components/ui/container";
+import Billboard from "../components/billboard";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const billboard = await getBillboard("1");
+
     return ( 
-        <div>Hello Store</div>
+        <div className="space-y-10 pb-10">
+            <Billboard data={billboard}/>
+        </div>
      );
 }
  
